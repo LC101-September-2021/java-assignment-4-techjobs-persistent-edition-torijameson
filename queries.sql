e4f47b67132889be87ae11537570dd8e7942dc41
torijameson <vtjameson@gmail.com>
2022-03-19 12:18:01 -0500

    This is totally done. I cant even believe it

--- queries.sql
+++ queries.sql
@@ -1,13 +1,13 @@
 -- Part 1: Test it with SQL
 DESCRIBE job;
 -- Part 2: Test it with SQL
-SELECT employers FROM techjobs
+SELECT name FROM employer
 WHERE Location = "St. Louis City";
+
 -- Part 3: Test it with SQL
-DROP TABLE jobs;
+DROP TABLE job;
 -- Part 4: Test it with SQL
-SELECT skills,
-       jobs
-FROM techjobs
-WHERE jobs IS NOT NULL
-ORDER BY skills ASC;
+SELECT * FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,6 +1,7 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
@@ -11,17 +12,30 @@ import java.util.Set;
 public class Skill extends AbstractEntity {
 
     @Size(max=512, message="PLce description of the skill desired here")
-    private String skillDescription;
+    private String description;
 
-    @ManyToMany
+    @ManyToMany (mappedBy = "skills")
     private List<Job> jobs = new ArrayList<>();
 
     public Skill(){}
 
     public Skill(String description){
-        this.skillDescription=description;
+        this.description=description;
     }
 
+    public String getDescription() {
+        return description;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
